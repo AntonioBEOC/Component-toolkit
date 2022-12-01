4f291b4cbfb1639a36645f1e397357f9834b1917
Benitez <antbenitez@deloitte.com>
2022-12-01 01:53:00 -0600

    added 3 diffrent button states

--- button.js
+++ button.js
@@ -2,25 +2,67 @@ class button extends HTMLElement {
     constructor() {
         super();
         console.log('component mounted');
+
+        this.attachShadow({mode: 'open'});
     };
 
-    connectedCallback() {
+    connectedCallback() {    
+        //styles
+        const style = document.createElement('link');
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('href', 'styles.css');
+
+        this.shadowRoot.append(style);
+
+        //icon 
 
         const button = document.createElement('button');
 
-        if(this.hasAttribute('text')){
-            button.textContent = this.getAttribute('text')
-        }else{
-            button.textContent = 'accept'
+        let classes  ='';
+
+        //label or no label-----------------------------------------------------------
+        if(this.hasAttribute('label')){
+            
+            button.textContent = this.getAttribute('label')
+            classes = classes + ' label';
         }
 
-        button.addEventListener('mouseover', this._highlight.bind(this));
-        this.appendChild(button);
+        //icon or no icon;
+        if (this.hasAttribute('icon')){
+
+            const icon = document.createElement('img');
+            icon.setAttribute('src', this.getAttribute('icon'));
+            button.appendChild(icon);
+        }
+
+        //alternative button
+        if(this.hasAttribute('secondary')){
+            classes = classes + ' secondary';
+        }else if(this.hasAttribute('tertiary')){
+            classes = classes + ' tertiary';
+        } else {
+            classes = classes + ' main';
+        }
+
+        //disabled state
+        if(this.hasAttribute('disabled')){
+            classes = classes + ' disabled';
+        }
+
+        button.setAttribute('class', classes)
+
+        button.addEventListener('click', this._press.bind(this));
+        this.shadowRoot.appendChild(button);
+        this.style.position = 'relative';
     }
 
-    _highlight() {
-        console.log('a');
+    _press() {
+        console.log(this)
     }
+
+
+
+
 };
 
 customElements.define('toolkit-button', button);
